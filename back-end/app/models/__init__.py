4627f2d110858f4179f14221dbb973dde6731973
hirtonsantos <hirtonsantos.hs@gmail.com>
2022-04-12 19:58:59 -0300

    feat: add heroku

--- back-end/app/models/__init__.py
+++ back-end/app/models/__init__.py
@@ -3,6 +3,7 @@ import os
 
 configs = {
     "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
     "database": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD")
